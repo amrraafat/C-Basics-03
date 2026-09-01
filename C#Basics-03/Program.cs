3cdf08334a45eb705c1efdfff46b46da5991b1c2
AmrRaafat <amrraafat>
2026-09-01 17:32:28 +0300

    Add project files.

---
+++ C#Basics-03/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Question 1
+// Create a book and put it in an object variable then print it
+
+Book b1 = new Book();
+b1.Title = "C# Guide";
+b1.Pages = 150;
+
+// Store book in object variable
+object myBook = b1;
+
+Console.WriteLine(myBook);
+#endregion
+
+// Simple Book class
+public class Book
+{
+    public string Title;
+    public int Pages;
+}
